9b9c8b5bca099f292bafe3d3487643c57b8f2fef
Mahmoud <mymafm0405@gmail.com>
2022-04-16 12:08:27 +0300

    adding some routes and tabs

--- src/app/places/offers/offers-routing.module.ts
+++ src/app/places/offers/offers-routing.module.ts
@@ -15,7 +15,11 @@ const routes: Routes = [
   {
     path: 'edit-offer',
     loadChildren: () => import('./edit-offer/edit-offer.module').then( m => m.EditOfferPageModule)
+  },  {
+    path: 'offer-bookings',
+    loadChildren: () => import('./offer-bookings/offer-bookings.module').then( m => m.OfferBookingsPageModule)
   }
+
 ];
 
 @NgModule({
