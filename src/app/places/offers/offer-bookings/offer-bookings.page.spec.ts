e3bca8bc5b9b85a4c2699ff01dc4e49ee67fe55d
Mahmoud Sliman <mahmoud@Mahmouds-MBP.hotspot>
2022-04-17 10:32:52 +0300

    SOME CHANGES

--- src/app/places/offers/offer-bookings/offer-bookings.page.spec.ts
+++ src/app/places/offers/offer-bookings/offer-bookings.page.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { HomePage } from './home.page';
+import { OfferBookingsPage } from './offer-bookings.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('OfferBookingsPage', () => {
+  let component: OfferBookingsPage;
+  let fixture: ComponentFixture<OfferBookingsPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ OfferBookingsPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(OfferBookingsPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
