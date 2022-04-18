fcecd98551bd4e06168eb3e6091b35c6646ba648
Mahmoud Sliman <mahmoud@Mahmouds-MacBook-Pro.local>
2022-04-18 13:55:52 +0300

    old

--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -7,50 +7,58 @@ const routes: Routes = [
   {
     path: '',
     component: PlacesPage,
-  },
-  {
-    path: 'discover',
-    loadChildren: () =>
-      import('./discover/discover.module').then((m) => m.DiscoverPageModule),
     children: [
       {
-        path: ':placeId',
-        loadChildren: () =>
-          import('./discover/place-details/place-details.module').then(
-            (m) => m.PlaceDetailsPageModule
-          ),
+        path: 'discover',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('./discover/discover.module').then((m) => m.DiscoverPageModule),
+          },
+          {
+            path: ':placeId',
+            loadChildren: () =>
+              import('./discover/place-details/place-details.module').then(
+                (m) => m.PlaceDetailsPageModule
+              ),
+          },
+        ],
       },
-    ],
+      {
+        path: 'offers',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('./offers/offers.module').then((m) => m.OffersPageModule),
+          },
+          {
+            path: 'new',
+            loadChildren: () =>
+              import('./offers/new-offer/new-offer.module').then(
+                (m) => m.NewOfferPageModule
+              ),
+          },
+          {
+            path: 'edit/:placeId',
+            loadChildren: () =>
+              import('./offers/edit-offer/edit-offer.module').then(
+                (m) => m.EditOfferPageModule
+              ),
+          },
+          {
+            path: ':placeId',
+            loadChildren: () =>
+              import('./offers/offer-bookings/offer-bookings.module').then(
+                (m) => m.OfferBookingsPageModule
+              ),
+          },
+        ],
+      },
+    ]
   },
-  {
-    path: 'offers',
-    loadChildren: () =>
-      import('./offers/offers.module').then((m) => m.OffersPageModule),
-    children: [
-      {
-        path: 'new',
-        loadChildren: () =>
-          import('./offers/new-offer/new-offer.module').then(
-            (m) => m.NewOfferPageModule
-          ),
-      },
-      {
-        path: 'edit/:placeId',
-        loadChildren: () =>
-          import('./offers/edit-offer/edit-offer.module').then(
-            (m) => m.EditOfferPageModule
-          ),
-      },
-      {
-        path: ':placeId',
-        loadChildren: () =>
-          import('./offers/offer-bookings/offer-bookings.module').then(
-            (m) => m.OfferBookingsPageModule
-          ),
-      },
-    ],
-  },
-];
+    ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
